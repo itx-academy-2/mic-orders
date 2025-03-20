642243509f463496886f3ef4d30112008b11643c
Vitalii Fedyk <vfedyk@softserveinc.com>
2025-03-20 11:01:23 +0200

    bugfix: add links to article contents

---
+++ code/orders-boot/src/main/resources/db/migration/V1.39__addLinksToArticleContents.sql
@@ -0,0 +1,15 @@
+UPDATE article_contents
+SET content = 'Finding the perfect gadget can be a daunting task, but we''re here to help. Start by reading our detailed product descriptions, which provide valuable insights into the features and specifications of each item. This information will help you understand what each gadget offers and how it can meet your needs.
+
+Utilize our website''s filtering and sorting features to narrow down your options. You can filter products based on categories, price ranges, and specific features, making it easier to find gadgets that match your preferences. Sorting options also allow you to prioritize products by popularity, ratings, or newest arrivals.
+
+Don''t forget to check out our <a href="/#bestsellers-section">Bestseller section</a>, where you''ll find some of the most popular and highly-rated gadgets. These products have been tried and tested by other customers, giving you an idea of their performance and reliability. Additionally, explore our <a href="/products/sales">Sales page</a> for exciting discounts and special offers on a wide range of gadgets. '
+WHERE article_id = 4 AND language_id = 1;
+
+UPDATE article_contents
+SET content = 'Знайти ідеальний гаджет може бути непростим завданням, але ми тут, щоб допомогти. Почніть з читання наших детальних описів продуктів, які надають цінну інформацію про характеристики та специфікації кожного товару. Ця інформація допоможе вам зрозуміти, що пропонує кожен гаджет і як він може задовольнити ваші потреби.
+
+Використовуйте функції фільтрації та сортування на нашому веб-сайті, щоб звузити вибір. Ви можете фільтрувати продукти за категоріями, ціновими діапазонами та конкретними характеристиками, що полегшить пошук гаджетів, які відповідають вашим уподобанням. Опції сортування також дозволяють пріоритезувати продукти за популярністю, рейтингами або новинками.
+
+Не забудьте перевірити наш розділ <a href="/#bestsellers-section">"Бестселери"</a>, де ви знайдете деякі з найпопулярніших і високо оцінених гаджетів. Ці продукти були випробувані іншими клієнтами, що дає вам уявлення про їх продуктивність і надійність. Крім того, ознайомтеся з нашою <a href="/products/sales">сторінкою розпродажів</a>, де ви знайдете захоплюючі знижки та спеціальні пропозиції на широкий асортимент гаджетів. '
+WHERE article_id = 4 AND language_id = 2;
