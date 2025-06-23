1b3d3ac15d0f862994524810988e2cc1c9213c5c
ChernenkoVitaliy <Chernenko_vitaliy@ukr.net>
2025-06-23 20:00:42 +0300

    Fixed @ActiveProfile value for the AbstractRepositoryIT. Replaced 'IT'(uppercase) with 'it'(lowercase); (#49)
    Added missed bean MeterRegistry into the AbstractRepositoryIT;

--- code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/common/repository/AbstractRepositoryIT.java
+++ code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/common/repository/AbstractRepositoryIT.java
@@ -1,5 +1,6 @@
 package com.academy.orders.boot.infrastructure.common.repository;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -8,9 +9,11 @@ import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("IT")
+@ActiveProfiles("it")
 public abstract class AbstractRepositoryIT {
   @MockBean
   private SecurityFilterChain securityFilterChain;
 
+  @MockBean
+  private MeterRegistry meterRegistry;
 }
