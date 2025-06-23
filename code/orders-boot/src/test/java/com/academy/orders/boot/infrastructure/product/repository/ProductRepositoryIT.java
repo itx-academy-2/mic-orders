9276e00f8889484bf4c8bf45c3b97822ffb5c7d6
Chernenko Vitaliy <chernenko_vitaliy@ukr.net>
2025-06-23 19:52:41 +0300

    Removed unused imports;

--- code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/product/repository/ProductRepositoryIT.java
+++ code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/product/repository/ProductRepositoryIT.java
@@ -36,8 +36,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 class ProductRepositoryIT extends AbstractRepositoryIT {
   @Autowired
