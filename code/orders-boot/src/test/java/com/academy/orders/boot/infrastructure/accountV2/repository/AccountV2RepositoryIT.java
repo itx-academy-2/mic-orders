71fa2efce2a6b3b99fe4f811e27093d4d634b47d
Chernenko Vitaliy <chernenko_vitaliy@ukr.net>
2025-07-01 17:22:38 +0300

    Formatted AccountV2RepositoryIT;

--- code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/accountV2/repository/AccountV2RepositoryIT.java
+++ code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/accountV2/repository/AccountV2RepositoryIT.java
@@ -10,31 +10,31 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AccountV2RepositoryIT extends AbstractRepositoryIT {
-    @Autowired
-    private AccountV2Repository accountV2Repository;
+  @Autowired
+  private AccountV2Repository accountV2Repository;
 
-    @Test
-    void findAccountByIdReturnsAccountWhenIdExistsTest() {
-        // Given
-        Long existingId = 1L;
+  @Test
+  void findAccountByIdReturnsAccountWhenIdExistsTest() {
+    // Given
+    Long existingId = 1L;
 
-        // When
-        Optional<AccountV2> result = accountV2Repository.findAccountById(existingId);
+    // When
+    Optional<AccountV2> result = accountV2Repository.findAccountById(existingId);
 
-        // Then
-        assertThat(result).isPresent();
-        assertThat(result.get().id()).isEqualTo(existingId);
-    }
+    // Then
+    assertThat(result).isPresent();
+    assertThat(result.get().id()).isEqualTo(existingId);
+  }
 
-    @Test
-    void findAccountByIdReturnEmptyWhenIdNotExistsTest() {
-        // Given
-        Long nonExistingId = 999L;
+  @Test
+  void findAccountByIdReturnEmptyWhenIdNotExistsTest() {
+    // Given
+    Long nonExistingId = 999L;
 
-        // When
-        Optional<AccountV2> result = accountV2Repository.findAccountById(nonExistingId);
+    // When
+    Optional<AccountV2> result = accountV2Repository.findAccountById(nonExistingId);
 
-        // Then
-        assertThat(result).isNotPresent();
-    }
+    // Then
+    assertThat(result).isNotPresent();
+  }
 }
