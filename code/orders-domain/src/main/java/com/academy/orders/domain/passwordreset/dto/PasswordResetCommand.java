2766ecb759bd59b32f1455c2aae8fdec2fcffc45
SonnyRose <keeganwunder0@gmail.com>
2025-07-24 12:45:31 +0300

    formatted

--- code/orders-domain/src/main/java/com/academy/orders/domain/passwordreset/dto/PasswordResetCommand.java
+++ code/orders-domain/src/main/java/com/academy/orders/domain/passwordreset/dto/PasswordResetCommand.java
@@ -4,12 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record PasswordResetCommand(
-    @NotBlank(message = "Token cannot be blank")
-    String token,
+    @NotBlank(message = "Token cannot be blank") String token,
 
-    @NotBlank(message = "Password cannot be blank")
-    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
-    String password) {
+    @NotBlank(message = "Password cannot be blank") @Size(min = 8, max = 64,
+        message = "Password must be between 8 and 64 characters") String password) {
 }
-
-
