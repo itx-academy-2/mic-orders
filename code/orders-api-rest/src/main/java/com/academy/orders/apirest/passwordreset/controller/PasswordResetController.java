ae38a58cda4eb986ae134c8ebcac28de99f2fd40
SonnyRose <keeganwunder0@gmail.com>
2025-07-24 13:43:28 +0300

    deleted cross origin annotation

--- code/orders-api-rest/src/main/java/com/academy/orders/apirest/passwordreset/controller/PasswordResetController.java
+++ code/orders-api-rest/src/main/java/com/academy/orders/apirest/passwordreset/controller/PasswordResetController.java
@@ -18,13 +18,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin
 public class PasswordResetController implements PasswordResetControllerApi {
   private final ProcessPasswordResetUseCase processPasswordResetUseCase;
 
