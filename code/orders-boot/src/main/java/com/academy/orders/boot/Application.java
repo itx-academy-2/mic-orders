d6b0628706c62427718682646b78d16c0e48261d
Vitalii Chernenko <vcher4@softserveinc.com>
2025-11-20 21:51:29 +0200

    refactor: add validation in the PexelsClientProperties

--- code/orders-boot/src/main/java/com/academy/orders/boot/Application.java
+++ code/orders-boot/src/main/java/com/academy/orders/boot/Application.java
@@ -1,16 +1,15 @@
 package com.academy.orders.boot;
 
-import com.academy.orders.boot.config.UsersConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class, TomcatMetricsAutoConfiguration.class})
 @ComponentScan(basePackages = "com.academy.orders")
-@EnableConfigurationProperties(UsersConfig.class)
+@ConfigurationPropertiesScan
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
